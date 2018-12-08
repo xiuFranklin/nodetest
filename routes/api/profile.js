79ec4d875d67e864919245abb5f5cc2e4c740938
xiuFranklin <1426618405_k@sina.com>
2018-12-08 10:10:30 +0800

    1.修复编辑信息、删除信息异常的bug,主要是id字段写错导致；2.第十六课

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -63,5 +63,47 @@ router.get("/:id",passport.authenticate("jwt",{session:false}),(req,res) => {
 
 })
 
+//编辑信息接口
+router.post("/edit/:id",passport.authenticate("jwt",{session:false}),(req,res) => {
+    
+    const profileFiels = {};
+    if(req.body.type) profileFiels.type = req.body.type;
+    if(req.body.describe) profileFiels.describe = req.body.describe;
+    if(req.body.income) profileFiels.income = req.body.income;
+    if(req.body.expend) profileFiels.expend = req.body.expend;
+    if(req.body.cash) profileFiels.cash = req.body.cash;
+    if(req.body.remark) profileFiels.remark = req.body.remark;
+    
+
+    // Profile.findByIdAndUpdate(
+    //     {_id: req.param.id},
+    //     {$set: profileFiels},
+    //     {new: true}
+    // ).then(profile => res.json(profile));
+
+    //这里的id，在视频中写错了
+    Profile.findOneAndUpdate(
+        {id: req.param.id},
+        {$set: profileFiels},
+        {new: true}
+    ).then(profile => res.json(profile));
+});
+
+
+// 删除信息接口
+router.delete('/delete/:id',passport.authenticate("jwt",{session:false}),(req,res) => {
+
+    Profile.findOneAndRemove({_id:req.params.id})
+          .then(profile => {
+            console.log(req.params.id);
+              profile.save().then(profile => res.json(profile));
+          })
+          .catch(err => res.json("删除失败")); 
+
+})
+
+
+
+
 
 module.exports = router
